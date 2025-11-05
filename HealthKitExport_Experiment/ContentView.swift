762125942c4af57fc3002cdfa6b752bb2c405265
Dominik Liehr <dominik@liehr.dev>
2025-11-05 08:16:30 +0100

    Read workouts and heart rate data

--- HealthKitExport_Experiment/ContentView.swift
+++ HealthKitExport_Experiment/ContentView.swift
@@ -6,56 +6,215 @@
 //
 
 import SwiftUI
-import SwiftData
+import HealthKit
+import DevTools
+import Charts
+import Algorithms
+
+struct Point: Identifiable {
+    let id: Int
+    let date: Date
+    let value: Double
+}
+
+@Observable
+class ViewModel {
+    private var healthStore: HKHealthStore?
+    
+    var data = [Point]()
+    var minY = Int.max
+    var maxY = Int.min
+    
+    var dateFrom = Date()
+    var dateTo = Date()
+    
+    // Neu: Speicher für Workouts
+    var workouts: [HKWorkout] = []
+    
+    init() {
+        guard HKHealthStore.isHealthDataAvailable() else {  fatalError("This app requires a device that supports HealthKit") }
+        
+        healthStore = HKHealthStore()
+    }
+    
+    func requestHealthkitPermissions() {
+        let sampleTypesToRead = Set([
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!,
+            HKObjectType.workoutType()
+        ])
+        
+        healthStore?.requestAuthorization(toShare: nil, read: sampleTypesToRead) { (success, error) in
+            print("Request Authorization -- Success: ", success, " Error: ", error ?? "nil")
+        }
+    }
+    
+    func readHeartRate(){
+        let quantityType  = HKObjectType.quantityType(forIdentifier: .heartRate)!
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        
+       
+        
+        let sampleQuery = HKSampleQuery.init(sampleType: quantityType,
+                                             predicate: get24hPredicate(),
+                                             limit: HKObjectQueryNoLimit,
+                                             sortDescriptors: [sortDescriptor],
+                                             resultsHandler: queryResultHandler(_:_:_:))
+        
+        self.healthStore?.execute(sampleQuery)
+    }
+    
+    func readWorkouts(){
+        let workoutType = HKObjectType.workoutType()
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let predicate = get24hPredicate()
+        
+        let query = HKSampleQuery(sampleType: workoutType,
+                                  predicate: predicate,
+                                  limit: HKObjectQueryNoLimit,
+                                  sortDescriptors: [sortDescriptor]) { [weak self] _, results, error in
+            guard let self else { return }
+            
+            if let error {
+                print("readWorkouts error:", error)
+                return
+            }
+            
+            guard let workouts = results as? [HKWorkout] else {
+                print("readWorkouts: results cast failed or nil")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.workouts = workouts
+            }
+        }
+        
+        healthStore?.execute(query)
+    }
+    
+    private func queryResultHandler(_ query: HKSampleQuery, _ results: [HKSample]?, _ error: (any Error)?) {
+        guard let samples = results as? [HKQuantitySample] else {
+            print(error!)
+            return
+        }
+        
+        var newData = [Point]()
+        
+        var productTypes = Set<String>()
+        var productTypesNames = Set<String>()
+        var sources = Set<HKSourceRevision>()
+        
+        for (i,sample) in samples.reversed().enumerated() {
+//            let mSample = sample.quantity.doubleValue(for: HKUnit(from: "count/min"))
+            
+            let device = sample.device
+            
+            let value = sample.quantity.doubleValue(for: .count().unitDivided(by: .minute()))
+            
+            let value2 = sample.quantity.doubleValue(for: .count().unitDivided(by: .second()))
+            
+            let startDate = sample.startDate.asString("HH:mm:ss.SSS")
+            let endDate = sample.endDate.asString("HH:mm:ss.SSS")
+            
+            let meta = sample.metadata
+            let source = sample.sourceRevision
+            
+            if let productType = source.productType {
+                productTypes.insert(productType)
+            }
+            
+            productTypesNames.insert(source.source.name)
+            sources.insert(source)
+            
+            newData.append(Point(id: i, date: sample.startDate, value: value))
+        }
+        
+        data.removeAll()
+        
+        let min = newData.min { p0, p1 in
+            p0.value < p1.value
+        }
+        
+        let max = newData.max { p0, p1 in
+            p0.value < p1.value
+        }
+        
+        self.minY = Int(min?.value ?? 0.0)
+        self.maxY = Int(max?.value ?? 200.0)
+        
+        DispatchQueue.main.async {
+            self.data = newData
+        }
+    }
+    
+    private func get24hPredicate() ->  NSPredicate{
+//        let startDate = Date(timeIntervalSince1970: TimeInterval(1762210800))
+//        let endDate = Date(timeIntervalSince1970: TimeInterval(1762293600))
+        
+        let predicate = HKQuery.predicateForSamples(withStart: dateFrom, end: dateTo, options: [])
+        
+        return predicate
+    }
+}
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State private var vm = ViewModel()
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ScrollView {
+            VStack(spacing: 16) {
+                DatePicker("Von", selection: $vm.dateFrom)
+                DatePicker("Bis", selection: $vm.dateTo)
+                
+                Button {
+                    vm.requestHealthkitPermissions()
+                } label: {
+                    Text("permission")
+                }
+                
+                Button {
+                    vm.readHeartRate()
+                } label: {
+                    Text("read heart rate")
+                }
+                
+                Button {
+                    vm.readWorkouts()
+                } label: {
+                    Text("read workouts")
+                }
+                
+                Text("#Workouts: \(vm.workouts.count)")
+                
+                if !vm.workouts.isEmpty {
+                    ForEach(vm.workouts) { elem in
+                        Text("Workout: \(elem.nameWithIndoor)")
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+                Chart(vm.data) { point in
+                    LineMark(
+                        x: .value("Uhrzeit", point.date),
+                        y: .value("BPM", point.value)
+                    )
+                    .foregroundStyle(Color.yellow)
+                }
+                .chartXAxisLabel(alignment: .center) {
+                    Text("Uhrzeit")
+                }
+                .chartYAxisLabel(position: .trailing) {
+                    Text("BPM")
+                }
+                .chartYScale(domain: [vm.minY, vm.maxY])
+                .frame(height: 200)
             }
+            .padding()
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ HealthKitExport_Experiment/Extensions/HKWorkout+name.swift
@@ -0,0 +1,21 @@
+//
+//  HKWorkout+name.swift
+//  HealthKitExport_Experiment
+//
+//  Created by Dominik on 05.11.25.
+//
+
+import Foundation
+import HealthKit
+
+extension HKWorkout: @retroactive Identifiable {
+    public var id: UUID { self.uuid }
+    
+    var nameWithIndoor: String {
+        let baseName = workoutActivityType.commonName
+        let isIndoor = (metadata?["HKIndoorWorkout"] as? NSNumber)?.boolValue == true
+        
+        return isIndoor ? "\(baseName) (Indoor)" : baseName
+    }
+    
+}
